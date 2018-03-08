d19f08142deeb0e7f49b6db15895838620b6b149
vieck <vieck@purdue.edu>
2018-03-07 22:03:26 -0500

    List view added.

--- lib/RandomWords.dart
+++ lib/RandomWords.dart
@@ -1,4 +1,5 @@
 import 'package:english_words/english_words.dart';
+import 'package:flutter/material.dart';
 import 'package:flutter/widgets.dart';
 class RandomWords extends StatefulWidget {
 
@@ -7,10 +8,43 @@ class RandomWords extends StatefulWidget {
 }
 
 class RandomWordsState extends State<RandomWords> {
+  final _suggestions = <WordPair>[];
+  final _saved = new Set<WordPair>();
+  final _bigFont = const TextStyle(fontSize: 18.0);
+
+  Widget _buildSuggestions() {
+    return new ListView.builder(
+      padding: const EdgeInsets.all(16.0),
+      itemBuilder: (context, i) {
+        if (i.isOdd) return new Divider();
+
+        final index = i ~/ 2;
+        if (index >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+        return _buildRow(_suggestions[index]);
+      },
+    );
+  }
+
+  Widget _buildRow(WordPair pair) {
+
+
+    return new ListTile(
+      title: new Text(
+        pair.asPascalCase,
+        style: _bigFont,
+      ),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    final wordPair = new WordPair.random();
-    return new Text(wordPair.asPascalCase);
+    return new Scaffold(
+      appBar: new AppBar(
+        title: new Text('Name Generator List'),
+      ),
+      body: _buildSuggestions(),
+    );
   }
 }
--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:english_words/english_words.dart';
 import 'package:flutter_tutorial/RandomWords.dart';
 
 void main() => runApp(new MyApp());
@@ -8,16 +7,9 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
-
-    final wordPair = new WordPair.random();
-
     return new MaterialApp(
-      title: 'Hello World In Flutter',
-      home: new Scaffold(
-          appBar: new AppBar(
-            title: new Text('Welcome to Flutter'),
-          ),
-          body: new Center(child: new RandomWords())),
+      title: 'Name Generator In Flutter',
+      home: new RandomWords(),
     );
   }
 }
